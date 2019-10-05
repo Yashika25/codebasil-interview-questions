d57af2dcca7b4ecd0138fb48a9c8f4d7fca7588c
YASHIKA KALRA <yashika99@ieee.org>
2019-10-05 22:58:39 +0530

    Update fibonacci.cpp

--- warmp-up-collection/Fibonacci-Series/fibonacci.cpp
+++ warmp-up-collection/Fibonacci-Series/fibonacci.cpp
@@ -1,4 +1,5 @@
 //Fibonacci Series using Recursion 
+
 #include<bits/stdc++.h> 
 using namespace std; 
 
@@ -7,8 +8,8 @@ int fib(int n);
 int main () 
 { 
 	int n; 
-  cout << "\nEnter N to find Nth term in the Fibonacci Series: ";
-  cin >> n;
+  	cout << "\nEnter N to find Nth term in the Fibonacci Series: ";
+  	cin >> n;
 	cout << endl << n << " value in the series is: " << fib(n); 
 	getchar(); 
 	return 0; 
@@ -18,8 +19,8 @@ int fib(int num)
 { 
 	if (num == 0) 
 		return 0; 
-  else if (num == 1)
-    return 1;
-  else
-	  return (fib(n-1) + fib(n-2)); 
+  	else if (num == 1)
+    		return 1;
+  	else
+	  	return (fib(n-1) + fib(n-2)); 
 }
